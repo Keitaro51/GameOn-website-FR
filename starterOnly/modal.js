70090e9decde7967ce35a5e016240855495a7c3f
R.Charlot <romain.charlot@lilo.org>
2021-06-08 09:22:37 +0200

    Forked project, doc uploaded and issue#1 resolved (modal closure)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
 
-// launch modal event
+// launch/close modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+//close modal
+function closeModal(){
+  modalbg.style.display = "none";
+}
