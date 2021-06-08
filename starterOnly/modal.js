902ecadc0091e0faa0fc720815c4123dde6ad54c
R.Charlot <romain.charlot@lilo.org>
2021-06-08 18:27:46 +0200

    Issue#2 : add regex pattern, required, and links between input and labels Issue#3: add custom invalid messages. Bug remaining with radio and checkbox buttons Some jsdocs implemented

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,3 +25,32 @@ function launchModal() {
 function closeModal(){
   modalbg.style.display = "none";
 }
+
+/**
+ * after a submitting valid form, store datas into a constant
+ *
+ * @return  {void}
+ */
+function validate(){
+  const formContent = {
+    'firstname' : document.getElementById('first').value,
+    'lastname' : document.getElementById('last').value,
+    'email' : document.getElementById('email').value,
+    'birthdate' : document.getElementById('birthdate').value,
+    'quantity' : document.getElementById('quantity').value,
+    'city' : cityChecked()
+  }
+}
+
+/**
+ * form selected city
+ * 
+ * @return  {string}  [return the selected city]
+ */
+function cityChecked(){
+  for(let i = 1; i<7; i++){
+    if(document.getElementById(`location${i}`).checked){
+      return document.getElementById(`location${i}`).value
+    }
+  }
+}
